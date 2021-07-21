23ba824d963848fd134cd7bd912d8535d84f50f8
Langsheng0716 <1083664623@qq.com>
2021-07-21 22:19:46 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,42 @@
+'''
+print("hello world!") # 字符串
+print(2333) # 整数
+print(2.3333) # 小数
+print(True) # 布尔
+print(()) # 元祖
+print([]) # 数组 
+print({}) # 字典
+锄禾日当午
+汗滴禾下土
+谁知盘中餐
+粒粒皆辛苦
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻")
+print("haha"*100)
+print(((100+2.89)-50)/2)
+'''
+'''a = float(input("请输入："))
+b = float(input("请输入："))
+print("输出的内容：",float(a+b))'''
+
+'''a = input()
+b = input()
+print("输入的值：",a+b)'''
+
+'''name = "张三"
+print (name)'''
+
+'''a = input () #只要是通过input输入的全是字符串，所以这里需要用到数据类型转换，type可以读取数据格式，例如type(2333) print(type(2333)) /// c = type(2333) print(c)
+print("input获取的内容:",a)'''   #str int float bool tuple list dict
+
+'''a = int(2333)
+print(type(2333))# class int  、、任何数据都可以转换为字符串 整数和小数可以互相转换 字符串转换为其他类型的数据 ，必须满足 【长得像】这个规律 哈哈哈三个字不能转换成int 一也不能转换成1'''
+#len 方法是计算长度 只支持字符串 元组数据字典
+''' a = 'asf'
+print(len(a))'''
+
+'''随机输入两个内容
+a = input("请输入：")
+b = input("请输入：")
+print("两个内容的长度之和是:",len(a)+len(b))
+计算他们两个内容的长度之和'''
