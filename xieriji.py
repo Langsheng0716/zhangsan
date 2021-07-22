35ba226e3983ffadf259e4751c7b577498e32f42
Langsheng0716y <1083664623@qq.com>
2021-07-23 02:35:44 +0800

    去去去

--- xieriji.py
+++ xieriji.py
@@ -3,6 +3,6 @@ import time
 now = time.strftime("%y-%m-%d %H:%M:%S")
 print(now)
 text = input("请输入今天的心情：")
-with open("日记.txt","a",encoding="utf8") as f:#w每次都是新的内容 而a是追加
+with open(。 "日记.txt","a",encoding="utf8") as f:#w每次都是新的内容 而a是追加
     f.write(now+"\n")
     f.write(text+"\n") #换行
